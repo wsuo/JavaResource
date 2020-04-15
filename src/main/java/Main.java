4b2f091ecb4fc241b415f964c73841439979d9f1
wsuo <ws2821@yeah.net>
2020-04-15 18:52:04 +0800

    初始化

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author shuo wang
+ * @Date 2020/4/15 0015 17:32
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
